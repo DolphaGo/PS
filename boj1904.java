129727b9a50b44f8ab3a6e03ac1ee4306fb32e7c
Dolphago <37108728+adamdoha@users.noreply.github.com>
2020-01-17 07:30:33 +0900

    01타일

---
+++ boj1904.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+		int dp[]=new int[1000001];
+		dp[1]=1; dp[2]=2;
+		for(int i=3;i<=n;i++) dp[i]=(dp[i-1]+dp[i-2])%15746;
+		System.out.println(dp[n]);
+	}
+}
