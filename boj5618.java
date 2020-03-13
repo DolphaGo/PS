d134fbc3268fca21bab38aa71141ef31167c8526
Dolphago <adamdoha@naver.com>
2020-03-13 17:10:38 +0900

    공약수

--- boj5618.java
+++ boj5618.java
@@ -1,25 +1,28 @@
 import java.util.*;
 import java.io.*;
 
-public class boj5618 {
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n=Integer.parseInt(br.readLine());
-		int data[]=new int[n];
 		StringTokenizer st=new StringTokenizer(br.readLine());
-		for(int i=0;i<n;i++) data[i]=Integer.parseInt(st.nextToken());
-		Arrays.sort(data);
-		StringBuilder sb=new StringBuilder();
-		for(int i=1;i<=data[0];i++) {
-			boolean flag=true;
-			for(int j=0;j<n;j++) {
-				if(data[j]%i!=0) {
-					flag=false;
-					break;
-				}
-			}
-			if(flag) sb.append(i+"\n");
+		int g=Integer.parseInt(st.nextToken());
+		while(st.hasMoreTokens()) {
+			int num=Integer.parseInt(st.nextToken());
+			g=gcd(g,num);
 		}
+		PriorityQueue<Integer> q=new PriorityQueue<>();
+		StringBuilder sb=new StringBuilder();
+		for(int i=1;i*i<=g;i++) {
+			if(g%i==0) {
+				q.add(i);
+				if(i*i!=g) q.add(g/i);
+			}
+		}
+        while(!q.isEmpty()) sb.append(q.poll()+"\n");
 		System.out.println(sb);
 	}
+    static int gcd(int a,int b){
+        return b==0?a:gcd(b,a%b);
+    }
 }
