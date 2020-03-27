f5ac18529f61f867ac7143341c747f6a0ae5ca95
adamdoha <adamdoha@naver.com>
2020-03-28 05:05:46 +0900

    수학은 체육과목입니다

---
+++ boj15894.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class boj15894 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long n=Long.parseLong(br.readLine());
+		long answer=4*n;
+		System.out.println(answer);
+	}
+}
