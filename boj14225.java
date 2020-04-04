15276800e2989e992fa8f41926095130210baf0a
adamdoha <adamdoha@naver.com>
2020-04-04 15:22:37 +0900

    문자열집합

---
+++ boj14225.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class boj14225 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		int n=Integer.parseInt(st.nextToken());
+		int m=Integer.parseInt(st.nextToken());
+		HashMap<String, Boolean> map=new HashMap<String, Boolean>();
+		for(int i=0;i<n;i++) {
+			String s=br.readLine();
+			map.put(s,true);
+		}
+		int answer=0;
+		for(int i=0;i<m;i++) {
+			String s=br.readLine();
+			if(map.get(s)==null) continue;
+			++answer;
+		}
+		System.out.println(answer);
+	}
+}
