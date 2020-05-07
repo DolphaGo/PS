0324d041b48ad5b7ed2e1e0872c4c6ff08e2a3dd
Dolphago <adamdoha@naver.com>
2020-05-07 15:40:35 +0900

    부분수열의합

--- boj14225.java
+++ boj14225.java
@@ -1,23 +1,31 @@
-import java.io.*;
 import java.util.*;
-
-public class boj14225 {
+import java.io.*;
+public class Main {
+	static HashMap<Integer,Boolean> map=new HashMap<>();
+	static int n;
+	static int[] arr;
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(br.readLine());
-		int n=Integer.parseInt(st.nextToken());
-		int m=Integer.parseInt(st.nextToken());
-		HashMap<String, Boolean> map=new HashMap<String, Boolean>();
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		n=Integer.parseInt(br.readLine());
+		arr=new int[n];
+		st=new StringTokenizer(br.readLine());
 		for(int i=0;i<n;i++) {
-			String s=br.readLine();
-			map.put(s,true);
+			arr[i]=Integer.parseInt(st.nextToken());
 		}
-		int answer=0;
-		for(int i=0;i<m;i++) {
-			String s=br.readLine();
-			if(map.get(s)==null) continue;
-			++answer;
+		
+		go(0,0);
+		for(int i=1;;i++) {
+			if(map.get(i)==null) {
+				System.out.println(i);
+				return;
+			}
 		}
-		System.out.println(answer);
+	}
+	static void go(int v,int sum) {
+		if(map.get(sum)==null) map.put(sum,true);
+		if(v==n) return;
+		go(v+1,sum+arr[v]);
+		go(v+1,sum);
 	}
 }
