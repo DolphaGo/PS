8af138a93be2a8901daba05e0ad8d2426d00b2cc
Dolphago <adamdoha@naver.com>
2020-05-15 01:22:28 +0900

    line01

---
+++ line01.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class line01 {
+	static String inputString="([뭐야}(>)ㅠㅠ()";
+	public static void main(String[] args) throws IOException {
+		System.out.println(solution(inputString));
+	}
+
+	static ArrayList<ArrayList<String>> list = new ArrayList<>();
+
+	static int solution(String inputString) {
+		Stack<Integer> stack=new Stack<>();
+		int cnt=0;
+		boolean flag=true;
+		for(int i=0;i<inputString.length();i++) {
+			char c=inputString.charAt(i);
+			if(c=='('||c=='['||c=='{'||c=='<') stack.add(1);
+			else if(c==')'||c==']'||c=='}'||c=='>') {
+				if(stack.size()==0) {
+					flag=false;
+					break;
+				}else {
+					stack.pop();
+					++cnt;
+				}
+			}
+		}
+		
+		if(flag) return cnt;
+		else return -1;
+	}
+}
