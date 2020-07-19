1ba6795635a06cb905b1a93523a4c82ee6695045
Dolphago <adamdoha@naver.com>
2020-07-20 00:30:29 +0900

    현주의 상자 바꾸기

---
+++ swea5789.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+class swea5789 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int T=Integer.parseInt(br.readLine());
+		StringBuilder sb=new StringBuilder();
+		for(int tc=1;tc<=T;tc++) {
+			st=new StringTokenizer(br.readLine());
+			int N=Integer.parseInt(st.nextToken());
+			int Q=Integer.parseInt(st.nextToken());
+			int[] arr=new int[N];
+			for(int i=1;i<=Q;i++) {
+				st=new StringTokenizer(br.readLine());
+				int L=Integer.parseInt(st.nextToken())-1;
+				int R=Integer.parseInt(st.nextToken())-1;
+				for(int j=L;j<=R;j++) arr[j]=i;
+			}
+			sb.append("#"+tc+" ");
+			for(int i=0;i<N;i++) sb.append(arr[i]+" ");
+			sb.append("\n");
+		}
+		System.out.print(sb);
+	}
+	static int pow(int a,int b) {
+		return b==0?1:a*pow(a,b-1);
+	}
+}
