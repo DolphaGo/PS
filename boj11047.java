82022eb51835ea79fb08de49440683aefdd0cb57
Dolphago <adamdoha@naver.com>
2020-07-20 22:44:39 +0900

    동전0

---
+++ boj11047.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+public class boj11047{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		int n=Integer.parseInt(st.nextToken());
+		int k=Integer.parseInt(st.nextToken());
+		int[] coin=new int[n];
+		for(int i=0;i<n;i++) coin[i]=Integer.parseInt(br.readLine());
+		
+		int answer=0;
+		for(int i=n-1;i>=0;i--) {
+			if(k>=coin[i]) {
+				answer += k/coin[i];
+				k%=coin[i];
+			}
+		}
+		System.out.println(answer);
+	}
+}
