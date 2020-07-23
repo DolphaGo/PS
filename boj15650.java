f0627bb3e0b531b1f14ea7ea64e6c6a0da4a5435
Dolphago <adamdoha@naver.com>
2020-07-24 00:45:01 +0900

    N과 M(2)

---
+++ boj15650.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class boj15650 {
+	static int n, m;
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[m];
+		go(0, 1);
+		System.out.print(sb.toString());
+	}
+
+	static void go(int v, int start) {
+		if (v == m) {
+			for (int i = 0; i < m; i++)	sb.append(arr[i] + " ");
+			sb.append("\n");
+			return;
+		}
+		for (int i = start; i <= n; i++) {
+			arr[v] = i;
+			go(v + 1, i + 1);
+		}
+	}
+}
