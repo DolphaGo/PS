e8b930fd0ee0c3af1a1c702a26084bdf479520a8
Dolphago <adamdoha@naver.com>
2020-07-30 00:09:59 +0900

    긴자리 계산

---
+++ boj2338.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class boj2338 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BigInteger A=new BigInteger(br.readLine());
+		BigInteger B=new BigInteger(br.readLine());
+		System.out.println(A.add(B));
+		System.out.println(A.subtract(B));
+		System.out.println(A.multiply(B));
+	}
+}
