60aa0549b0887dc8747acf8c87f14bc13fd6f572
Dolphago <adamdoha@naver.com>
2020-08-31 22:17:42 +0900

    상자넣기

---
+++ boj1965.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[] arr,dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n=Integer.parseInt(br.readLine());
+        st=new StringTokenizer(br.readLine());
+        arr=new int[n];
+        dp=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        int answer=0;
+        for(int i=0;i<n;i++){
+            if(dp[i]==0){
+                answer=Math.max(answer,go(i));
+            }
+        }
+        System.out.println(answer);
+    }
+    static int go(int v){
+        if(dp[v]!=0) return dp[v];
+        dp[v]=1;
+        for(int i=v+1;i<n;i++){
+            if(arr[v]<arr[i]){
+                dp[v]=Math.max(dp[v],go(i)+1);
+            }
+        }
+        return dp[v];
+    }
+}
