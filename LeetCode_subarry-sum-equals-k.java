7c6eeaae2be466e4713667e2ea69b567ac57ef0d
Dolphago <adamdoha@naver.com>
2020-11-18 13:56:42 +0900

    Subarray Sum Equals K

--- LeetCode_subarry-sum-equals-k.java
+++ LeetCode_subarry-sum-equals-k.java
@@ -1,12 +1,16 @@
-import java.io.*;
 import java.util.*;
 
-class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int h=Integer.parseInt(st.nextToken());
-        int n=Integer.parseInt(st.nextToken());
-        
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer,Integer> map=new HashMap<>();
+        map.put(0,1);
+        int sum=0,result=0;
+
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(map.containsKey(sum-k)) result+=map.get(sum-k);
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return result;
     }
 }
