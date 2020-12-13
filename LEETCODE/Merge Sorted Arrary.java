1abc0201c5181fc1685abd2eb02d8078c315a511
Dolphago <adamdoha@naver.com>
2020-12-14 03:18:06 +0900

    Merge Sorted Array

---
+++ LEETCODE/Merge Sorted Arrary.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=m-1,j=n-1,idx=m+n-1;
+        while(i>=0&&j>=0){
+            nums1[idx--]=nums1[i]<nums2[j]?nums2[j--]:nums1[i--];
+        }
+        while(i>=0) nums1[idx--]=nums1[i--];
+        while(j>=0) nums1[idx--]=nums2[j--];
+    }
+}
