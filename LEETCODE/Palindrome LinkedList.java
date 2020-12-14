c2a7c3af9d8de47721803120fcc18ba81b4b0c51
Dolphago <adamdoha@naver.com>
2020-12-15 04:24:46 +0900

    Palindrome LinkedList

---
+++ LEETCODE/Palindrome LinkedList.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if(fast != null) slow = slow.next;
+
+        slow = reverse(slow);
+        while(slow != null && head.val == slow.val) {
+            head = head.next;
+            slow = slow.next;
+        }
+        return slow == null;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while(head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+
+            head = next;
+        }
+        return prev;
+    }
+}
