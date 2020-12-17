51555f5e76fa2c7b80d9c72d7ba44d2a3e824a89
Dolphago <adamdoha@naver.com>
2020-12-18 01:40:46 +0900

    개미굴

--- BOJ/BOJ14725.java
+++ BOJ/BOJ14725.java
@@ -2,56 +2,66 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static class Node{
-        TreeMap<String,Node> map;
-        Node(){ map=new TreeMap<>();}
+    static class Node {
+        TreeMap<String, Node> map;
+        Node() {
+            map = new TreeMap<>();
+        }
     }
-    static class Trie{
-        Node root;
-        Trie(){ root=new Node(); }
 
-        void insert(String[] strings){
-            Node node=root;
-            for(String s:strings){
-                if(!node.map.containsKey(s)) node.map.put(s,new Node());
-                node=node.map.get(s);
+    static class Trie {
+        Node root;
+
+        Trie() {
+            root = new Node();
+        }
+
+        Node getRoot() {
+            return root;
+        }
+
+        void insert(String[] strings) {
+            Node node = root;
+            for (String s : strings) {
+                if (!node.map.containsKey(s)) node.map.put(s, new Node());
+                node = node.map.get(s);
             }
         }
-        public Node getRoot(){
-            return root;
+
+        void print(Node cur, int depth, StringBuilder sb) {
+            for (String s : cur.map.keySet()) {
+                makeDepth(depth, sb);
+                sb.append(s).append("\n");
+                Node next = cur.map.get(s);
+                print(next, depth + 2, sb);
+            }
+        }
+
+        void makeDepth(int depth, StringBuilder sb) {
+            while (depth-- > 0) sb.append('-');
         }
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        Trie trie=new Trie();
+        Trie trie = new Trie();
 
-        int n=Integer.parseInt(br.readLine());
-        for(int i=0;i<n;i++){
-            st=new StringTokenizer(br.readLine());
-            int k=Integer.parseInt(st.nextToken());
-            String[] strings=new String[k];
-            for(int j=0;j<k;j++){
-                strings[j]=st.nextToken();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int k = Integer.parseInt(st.nextToken());
+            String[] strings = new String[k];
+            for (int j = 0; j < k; j++) {
+                strings[j] = st.nextToken();
             }
             trie.insert(strings);
         }
 
-        Node root=trie.getRoot();
-        StringBuilder sb=new StringBuilder();
-        getDisplay(root,0,sb);
+        Node root = trie.getRoot();
+        StringBuilder sb = new StringBuilder();
+        trie.print(root, 0, sb);
         System.out.print(sb);
     }
-    static void getDisplay(Node cur,int depth,StringBuilder sb){
-        for(String s:cur.map.keySet()){
-            makeDepth(depth,sb);
-            sb.append(s).append("\n");
-            Node next=cur.map.get(s);
-            getDisplay(next,depth+2,sb);
-        }
-    }
-    static void makeDepth(int depth,StringBuilder sb){
-        while(depth-->0) sb.append('-');
-    }
+
 }
