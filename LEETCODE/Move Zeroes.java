96ed12dde94256bd0cded01bb56c53f648579b99
Dolphago <adamdoha@naver.com>
2020-12-21 03:10:35 +0900

    Move Zeroes

---
+++ LEETCODE/Move Zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int idx = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[idx++] = num;
+            }
+        }
+
+        while (idx < nums.length) {
+            nums[idx++] = 0;
+        }
+    }
+}
