4250eeb4ee2a3efc4af0745e44257dabb2139eac
adamdoha <adamdoha@naver.com>
2021-03-21 01:55:25 +0900

    북서풍_Bottom_up

--- BOJ/BOJ5419.java
+++ BOJ/BOJ5419.java
@@ -1,33 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] tree;
-    static int[][] arr;
-    static Integer[] ys;
+    static class Point {
+        int x, y;
 
-    static long query(int node, int start, int end, int left, int right) {
-        if (right < start || left > end) { return 0; }
-        if (left <= start && end <= right) {
-            return tree[node];
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
-        int m = (start + end) >> 1;
-        return query(node << 1, start, m, left, right) + query(node << 1 | 1, m + 1, end, left, right);
     }
 
-    static int update(int node, int start, int end, int idx) {
-        if (idx < start || idx > end) { return tree[node]; }
-        if (start == end) {
-            return tree[node] += 1;
+    static int[] tree;
+    static List<Point> list = new ArrayList<>();
+
+    static long query(int left, int right) {
+        long sum = 0;
+        while (left < right) {
+            if (left / 2 != (left + 1) / 2) {
+                sum += tree[left];
+            }
+            if (right / 2 != (right - 1) / 2) {
+                sum += tree[right];
+            }
+            left = (left + 1) / 2;
+            right = (right - 1) / 2;
         }
-        int m = (start + end) >> 1;
-        return tree[node] = update(node << 1, start, m, idx) + update(node << 1 | 1, m + 1, end, idx);
+        if (left == right) { sum += tree[left]; }
+        return sum;
+    }
+
+    static void update(int idx) {
+        while (idx > 0) {
+            tree[idx] += 1;
+            idx /= 2;
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -36,42 +50,45 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
         Map<Integer, Integer> map = new HashMap<>();
-
         for (int tc = 1; tc <= T; tc++) {
             int n = Integer.parseInt(br.readLine());
-            arr = new int[n][2];
-            ys = new Integer[n];
             for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
-                arr[i][0] = Integer.parseInt(st.nextToken());
-                arr[i][1] = Integer.parseInt(st.nextToken());
-                ys[i] = arr[i][1];
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                list.add(new Point(x, y));
             }
+            int[] arr=new int[4];
+            Arrays.sort(arr); // 느린 놈
 
-            // y 좌표는 내림차순으로 정렬
-            Arrays.sort(ys, Collections.reverseOrder());
+            list.sort((o1, o2) -> Integer.compare(o2.y, o1.y)); // 빠른 놈
             int size = 0;
             int prev = Integer.MIN_VALUE;
-            for (int y : ys) {
-                if (prev == y) { continue; }
-                map.put(y, ++size);
-                prev = y;
+            for (Point data : list) {
+                if (prev == data.y) { continue; }
+                map.put(data.y, ++size);
+                prev = data.y;
             }
+            // y 좌표는 내림차순으로 정렬
+            list.sort((o1, o2) -> {
+                if (o1.x == o2.x) { return Integer.compare(o2.y, o1.y); }
+                return Integer.compare(o1.x, o2.x);
+            });
 
-            Arrays.sort(arr, (o1, o2) -> {
-                if (o1[0] == o2[0]) { return Integer.compare(o2[1], o1[1]); }
-                return Integer.compare(o1[0], o2[0]);
-            }); // x좌표 순으로 정렬(쿼리용), x좌표가 같으면 북쪽에 있는 섬먼저.
+            int h = 1;
+            while (h < size) {
+                h *= 2;
+            }
+            tree = new int[h << 1];
 
-            int h = (int) Math.ceil(Math.log(size) / Math.log(2));
-            tree = new int[1 << h + 1];
             long answer = 0;
             for (int i = 0; i < n; i++) {
-                int val = map.get(arr[i][1]);
-                answer += query(1, 1, size, 1, val);
-                update(1, 1, size, val);
+                long query = query(h, h + map.get(list.get(i).y) -1 );
+                answer += query;
+                update(h + map.get(list.get(i).y) - 1);
             }
             sb.append(answer).append('\n');
+            list.clear();
             map.clear();
         }
         System.out.print(sb);
